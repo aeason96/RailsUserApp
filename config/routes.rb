70cb247a2745170a0775ef615f0c4e858c44ebcf
andrew <aeason96@vt.edu>
2018-08-09 16:50:01 +0000

    Basic login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   root 'static_pages#home'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
